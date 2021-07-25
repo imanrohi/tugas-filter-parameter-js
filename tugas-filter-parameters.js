d25206e881aca6f7ec47baf4ec996dbba3eb734b
User-niomic <user@niomic.com>
2021-07-25 23:25:18 +0700

    TUGAS FILTER PARAMETERS JAVASCRIPT
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas-filter-parameters.js
@@ -0,0 +1,15 @@
+function panggilFilterParameters(value) {
+  var arr = [
+    {negara: 'Indonesia', benua: 'Asia'},
+    {negara: 'Jerman', benua: 'Eropa'},
+    {negara: 'Spanyol', benua: 'Eropa'},
+    {negara: 'Korea', benua: 'Asia'},
+    {negara: 'Portugal', benua: 'Eropa'},
+    {negara: 'Amerika Serikat', benua: 'Amerika'},
+  ];
+  var benuaEropa = arr.filter(function(item) {
+      return item.benua === 'Eropa';
+  });
+  console.log(benuaEropa);
+}
+panggilFilterParameters();
